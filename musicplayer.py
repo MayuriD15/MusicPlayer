957c199c9c355382c60b9e36236f68f2b38919b5
Mayuri Atul Durugkar <93866846+MayuriD15@users.noreply.github.com>
2023-03-14 22:10:40 +0530

    Add files via upload

---
+++ musicplayer.py
@@ -0,0 +1,27 @@
+from playsound import playsound
+print("Enter Following no. for respective songs: \n1.Pehli Dafa \n2.Shiddat \n3.Chale Aana \n4.Kabhi Tumhe \n5.Aasmano-Pe-Jo-Khuda-Hai \n6.Suna-Hai \n7.Taaron Ke Shehar \n8.Tum Hi Aana \n9.Meri Aashiqui \n10.Hasi \n11.Aawara Shaam Hair \n12.Mera Pyar Tera Pyar") 
+order = input('Enter the music which you want to play:')
+if "1" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Pehli Dafa.mp3')
+elif "2" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Shiddat.mp3')
+elif "3" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Chale Aana.mp3')
+elif "4" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Kabhi Tumhe.mp3')
+elif "5" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Aasmano-Pe-Jo-Khuda-Hai.mp3')
+elif "6" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Suna-Hai.mp3')
+elif "7" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Taaron Ke Shehar.mp3')
+elif "8" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Tum Hi Aana.mp3')
+elif "9" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Meri Aashiqui.mp3')
+elif "10" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Hasi.mp3')
+elif "11" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Aawara Shaam Hair.mp3')
+elif "12" in order:
+    playsound('C:\\Users\\Admin\\Desktop\\Projects\\music player\\Mera Pyar Tera Pyar.mp3')
